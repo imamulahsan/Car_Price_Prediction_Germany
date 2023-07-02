197c072aed716d3d11a20c76d1f47de5181987ae
imamulahsan <imamulahsan@hotmail.com>
2023-07-02 20:46:07 +0200

    updated EDA and Model Train for Gemstone

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -49,10 +49,11 @@ class DataIngestion:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    obj = DataIngestion()
+    train_data, test_data = obj.initate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initate_data_transformation(train_data,test_data)
 
 
 
